41277d9ecea5d4a267e534d487af3590466fb002
Nishi <naalnishinelson@gmail.com>
2021-03-25 16:55:54 -0700

    phase 4 complete!

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -28,4 +28,25 @@ router.post(
     }),
   );
 
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;
