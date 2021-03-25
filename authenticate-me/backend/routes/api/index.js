bacd162ee5b6075548fa0dae7c22fcb8a492e349
Nishi <naalnishinelson@gmail.com>
2021-03-25 16:37:09 -0700

    more commits

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -3,7 +3,13 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   const user = await User.findOne({
