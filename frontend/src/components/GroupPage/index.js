93d1ba91db76acac6fb1761209f95a68d8e2eccc
Nishi <naalnishinelson@gmail.com>
2021-04-02 10:32:02 -0700

    groupPage functional

--- frontend/src/components/GroupPage/index.js
+++ frontend/src/components/GroupPage/index.js
@@ -3,16 +3,18 @@ import { useEffect } from 'react';
 import { getEvents } from '../../store/events';
 import { useParams } from 'react-router-dom';
 import EventCard from '../EventList/EventCard';
+import { getGroups } from '../../store/groups';
+import './GroupPage.css';
 
 function GroupPage () {
     const events = useSelector( state => Object.values(state.events));
-    const { groupId } = useParams();
-    console.log("HERE!!!:", groupId);
-    const group = useSelector( state => state.group[groupId]);
+    const { id } = useParams();
+    const group = useSelector( state => state.groups[id]) || {};
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getEvents())
+        dispatch(getGroups())
     }, [dispatch]);
 
     const renderEvents = events.map(event =>{
@@ -24,14 +26,26 @@ function GroupPage () {
     })
 
     return (
-        <div>
-            <div>{group.imageUrl}</div>
-            <div>{group.name}</div>
-            <div>{group.city}, {group.state}</div>
-            <div>{group.about}</div>
-            <ul>
-                {renderEvents}
-            </ul>
+        <div className='page'>
+            <div className='top-half'>
+                <img className='groupImg' src={group.imageUrl} alt='groupImg'/>
+                <div className='info'>
+                    <div className='name'>{group.name}</div>
+                    <div>{group.city}, {group.state}</div>
+                </div>
+            </div>
+            <div className='bottom-half'>
+                <div className='about'>
+                    <label>What we're all about:</label>
+                    <div>{group.about}</div>
+                </div>
+                <div className='events'>
+                    <label>Upcoming Events:</label>
+                    <div>
+                        {renderEvents}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
